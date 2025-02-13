426e118f69f2dc14dbdb0ce7705638926e20f3ad
Paulo Roberto <61876313+Maximuzsz@users.noreply.github.com>
2025-02-12 22:22:06 -0300

    correção no vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,20 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'; // Separa dependências externas em um chunk "vendor"
+          }
+          if (id.includes('components')) {
+            return 'components'; // Separa os componentes em outro chunk
+          }
+        },
+      },
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
